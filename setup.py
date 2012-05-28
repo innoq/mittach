bd98fce7239157feefd019e4a4a995513606a8d1
FND <fnd@fnd-oda.localdomain>
2012-05-28 15:07:42 +0200

    tweaked package contents

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 from setuptools import setup, find_packages
 
 
-NAME = "mittach"
+NAME = "mittach" # XXX: duplicates package metadata
 
 VERSION = open(os.path.join(os.path.dirname(__file__), NAME, "version.py"))
 exec VERSION.read() # initializes __version__
@@ -14,8 +14,9 @@ DESC = open(os.path.join(os.path.dirname(__file__), "README.md")).read()
 setup(name=NAME,
         version=__version__,
         long_description=DESC,
-        packages=find_packages(),
+        packages=find_packages(exclude=["test"]),
         include_package_data=True,
+        zip_safe=False,
         install_requires=["Flask", "redis"],
         extras_require = {
             "testing": ["pytest"],
