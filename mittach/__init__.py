d6b6b24486cb3bb78d2fa34ded915ffa36076e1b
FND <fnd@localhost.localdomain>
2012-06-21 15:38:02 +0200

    tweaked deployment script
    in theory, `touch`ing the WSGI app should suffice to use the new
    version, which would also mean `sudo` is no longer required (provided
    file permissions are set up correctly for the WSGI app) - we'll see
    whether this turns out to be correct

--- mittach/__init__.py
+++ mittach/__init__.py
@@ -4,5 +4,5 @@ innoQ lunch planning and registration
 https://github.com/innoq/mittach
 """
 
-__version__ = "0.7.1"
+__version__ = "0.7.2"
 # TODO: author, contributors, copyright, license and all that nonsense
